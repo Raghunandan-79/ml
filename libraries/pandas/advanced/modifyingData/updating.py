f6588de369db4da1fe7737f4260b5e2fd462a8ee
Raghunandan-79 <raghunandansharma1905@gmail.com>
2025-11-17 19:44:41 +0530

    Updating Columns

---
+++ libraries/pandas/advanced/modifyingData/updating.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = {
+    "Name":['Ram', "Shyam", "Ghanshyam", "Dhanshyam", "Aditi", "Jagdeesh", "Raj", "Simran"],
+    "Age":[28, 34, 22, 30, 29, 48, 25, 32],
+    "Salary":[50000, 40000, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_Score":[85, 90, 78, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+
+# .loc[] -> access specific location and do modification
+# df.loc[row_index, "Column_Name"] = new_value
+
+df.loc[0, "Salary"] = 55000
+print(df)
