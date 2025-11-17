5181de18b859f51a940378485093325d7e3e4734
Raghunandan-79 <raghunandansharma1905@gmail.com>
2025-11-17 20:02:34 +0530

    Handling

---
+++ libraries/pandas/advanced/handlingMissingData/handling.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {
+    "Name":['Ram', None, "Ghanshyam", "Dhanshyam", "Aditi", "Jagdeesh", "Raj", "Simran"],
+    "Age":[28, None, 22, 30, 29, 48, 25, 32],
+    "Salary":[50000, None, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_Score":[85, None, 78, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+"""
+    Dropping missing data: 
+    dropna()
+
+    df.dropna(axis=0, inplace=True) -> rows
+    df.dropna(axis=1, inplace=True) -> columns
+"""
+print()
+
+df.dropna(inplace=True)
+print(df)
---
+++ libraries/pandas/advanced/handlingMissingData/handling2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = {
+    "Name":['Ram', None, "Ghanshyam", "Dhanshyam", "Aditi", "Jagdeesh", "Raj", "Simran"],
+    "Age":[28, None, 22, 30, 29, 48, 25, 32],
+    "Salary":[50000, None, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_Score":[85, None, 78, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+
+"""
+    fillna()
+    fillna(value, inplace=True)
+"""
+
+# df.fillna(0, inplace=True)
+df['Age'].fillna(df["Age"].mean(), inplace=True)
+df['Age'].fillna(df["Salary"].mean(), inplace=True)
+print(df)
